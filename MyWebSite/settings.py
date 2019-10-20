129040c17270ba10d0ddd37aa199c940b3f9c2a5
GentleCP <574881148@qq.com>
2019-10-20 23:21:56 +0800

    simple function finished

--- MyWebSite/settings.py
+++ MyWebSite/settings.py
@@ -81,19 +81,19 @@ WSGI_APPLICATION = 'MyWebSite.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default':
-    #     {
-    #         'ENGINE': 'django.db.backends.sqlite3',
-    #         'NAME':     os.path.join(BASE_DIR, 'db.sqlite3'),
-    #     },
-    'default':{
-        'ENGINE':'django.db.backends.mysql',
-        'NAME':'mywebsite',
-        'USER':'root',
-        'PASSWORD':'chao5748',
-        'HOST':'127.0.0.1',
-        'PORT':'3306',
-    }
+    'default':
+        {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME':     os.path.join(BASE_DIR, 'db.sqlite3'),
+        },
+    # 'default':{
+    #     'ENGINE':'django.db.backends.mysql',
+    #     'NAME':'mywebsite',
+    #     'USER':'root',
+    #     'PASSWORD':'chao5748',
+    #     'HOST':'127.0.0.1',
+    #     'PORT':'3306',
+    # }
 }
 
 # Password validation
