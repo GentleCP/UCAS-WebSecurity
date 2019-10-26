72cf4404e5de420eb74064f526563a377c434798
Chary Liu <384735109@qq.com>
2019-10-26 15:02:04 +0800

    Add files via upload

--- user/models.py
+++ user/models.py
@@ -1,19 +1,6 @@
 from django.db import models
 
-
 # Create your models here.
-
-
-class HostInfo(models.Model):
-    """
-    访问的主机信息
-    """
-    host = models.CharField(max_length=32)  # 主机ip
-    count = models.IntegerField()  # 访问次数
-    start_time = models.DateTimeField()  # 第一次访问时间
-    is_lock = models.IntegerField(default=0)  # 是否限制,0：不限，1：受限
-
-
-
-
-
+class LoginCaptcha(models.Model):
+    username = models.CharField(max_length=32)
+    key = models.CharField(max_length=32)
--- user/views.py
+++ user/views.py
@@ -1,47 +1,65 @@
+import time
+
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.shortcuts import render,redirect
 from django.contrib import auth
 from django.urls import reverse
 from django.contrib.auth.models import User
-from MyWebSite.settings import LIMIT_TIME
+from django.views.decorators.csrf import csrf_exempt
 
-from .utils import limit_ip
+from user import models
 from .forms import LoginForm,RegisterForm
 
-# Create your views here.
-def login_limit_ip(request):
-    if request.META.get('HTTP_X_FORWARDED_FOR'):
-        ip = request.META.get("HTTP_X_FORWARDED_FOR")
-    else:
-        ip = request.META.get("REMOTE_ADDR")
+@csrf_exempt
+def slide_captcha(request):
+    if request.method == "POST":
+            username = request.POST.get('un')
+            res = request.POST.get('result')
+            t = str(time.time())
+            print(username)
+            print(res)
+            if res == 'true':
+                models.LoginCaptcha.objects.create(username=username, key= t)
+                # captcha.append({username:time.time()})
+                # print(captcha)
 
-    not_limit = True
-    context = {}
-    if request.method =="POST" and limit_ip(request):
-        not_limit = False  # 受限了不再进行post登录
-        context['limit_msg'] = '对不起，您的访问过于频繁，请等待%d秒后再操作！'% LIMIT_TIME
-    if not_limit and request.method == 'POST':
-        # 请求方法是POST类型，说明是登录请求
-        print(request.POST)
-        login_form = LoginForm(request.POST)  # 将request中的参数传入到Form 类中
-        if login_form.is_valid():
-            # 判断数据有效
-            user = login_form.cleaned_data['user']
-            auth.login(request,user)
-            return redirect(request.GET.get('referer'),reverse('index'))
+            dict = {'key': t,'username':username}
+            return JsonResponse(dict)
+
+
+# Create your views here.
+@csrf_exempt
+def login_slide_captcha(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        if models.LoginCaptcha.objects.filter(username=username).count() != 1 :
+            return HttpResponse('fail')
+        else:
+            user = models.LoginCaptcha.objects.get(username=username)
+            print(user)
+            if user:
+                if (user.key == request.POST.get('key')):
+                    models.LoginCaptcha.objects.get(username=request.POST.get('username')).delete()
+                    # 请求方法是POST类型，说明是登录请求
+                    login_form = LoginForm(request.POST)  # 将request中的参数传入到Form 类中
+                    print(request.POST)
+                    if login_form.is_valid():
+                        # 判断数据有效
+                        user = login_form.cleaned_data['user']
+                        auth.login(request, user)
+                        return HttpResponse('success')
+                        # return redirect(request.GET.get('referer'),reverse('index'))
     else:
         # 其他的方法，我们就刷新登录页面
         login_form = LoginForm()
 
-
+    context = {}
     context['form']=login_form
-    return render(request, 'login_limit_ip.html', context)
-
-
+    return render(request, 'login_slide.html', context)
 
 def login(request):
     if request.method == 'POST':
         # 请求方法是POST类型，说明是登录请求
-        print(request.POST)
         login_form = LoginForm(request.POST)  # 将request中的参数传入到Form 类中
         if login_form.is_valid():
             # 判断数据有效
